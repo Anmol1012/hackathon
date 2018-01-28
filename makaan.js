6dbad4700bedf7e2c85e3be77628aaaf479a1be0
Anmol <anmol.singhal1012@gmail.com>
2018-01-28 23:48:05 +0530

    Buy/Rent Filter

---
+++ makaan.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs=require('fs');
+var request = require('request');
+// const ejsLint= require('ejs-lint');
+var app = express();
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const url = 'http://www.makaan.com/petra/app/v4/listing?selector={"fields":["localityId","displayDate","listing","property","project","builder","displayName","locality","suburb","city","state","currentListingPrice","companySeller","company","user","id","name","label","listingId","propertyId","projectId","propertyTitle","unitTypeId","resaleURL","description","postedDate","verificationDate","size","measure","bedrooms","bathrooms","listingLatitude","listingLongitude","studyRoom","servantRoom","pricePerUnitArea","price","localityAvgPrice","negotiable","rk","buyUrl","rentUrl","overviewUrl","minConstructionCompletionDate","maxConstructionCompletionDate","halls","facingId","noOfOpenSides","bookingAmount","securityDeposit","ownershipTypeId","furnished","constructionStatusId","tenantTypes","bedrooms","balcony","floor","totalFloors","listingCategory","possessionDate","activeStatus","type","logo","profilePictureURL","score","assist","contactNumbers","contactNumber","isOffered","mainImageURL","mainImage","absolutePath","altText","title","imageCount","geoDistance","defaultImageId","updatedAt","qualityScore","projectStatus","throughCampaign","addedByPromoter","listingDebugInfo","videos","imageUrl","rk","penthouse","studio","paidLeadCount","listingSellerTransactionStatuses","allocation","allocationHistory","masterAllocationStatus","status","sellerCompanyFeedbackCount","companyStateReraRegistrationId"],"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"listingCategory":["Primary","Resale"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
+let json = '';
+request(url,function(err,response,body) {
+	json = JSON.parse(body);
+	//console.log(JSON.stringify(body));
+	console.log(json);
+
+});
+app.get('/', function (req, res) {
+	
+	res.render('index.ejs',{json:json.data[0].facetedResponse});
+});
+const renturl = 'http://www.makaan.com/petra/app/v4/listing?selector={"fields":["localityId","displayDate","listing","property","project","builder","displayName","locality","suburb","city","state","currentListingPrice","companySeller","company","user","id","name","label","listingId","propertyId","projectId","propertyTitle","unitTypeId","resaleURL","description","postedDate","verificationDate","size","measure","bedrooms","bathrooms","listingLatitude","listingLongitude","studyRoom","servantRoom","pricePerUnitArea","price","localityAvgPrice","negotiable","rk","buyUrl","rentUrl","overviewUrl","minConstructionCompletionDate","maxConstructionCompletionDate","halls","facingId","noOfOpenSides","bookingAmount","securityDeposit","ownershipTypeId","furnished","constructionStatusId","tenantTypes","bedrooms","balcony","floor","totalFloors","listingCategory","possessionDate","activeStatus","type","logo","profilePictureURL","score","assist","contactNumbers","contactNumber","isOffered","mainImageURL","mainImage","absolutePath","altText","title","imageCount","geoDistance","defaultImageId","updatedAt","qualityScore","projectStatus","throughCampaign","addedByPromoter","listingDebugInfo","videos","imageUrl","rk","penthouse","studio","paidLeadCount","listingSellerTransactionStatuses","allocation","allocationHistory","masterAllocationStatus","status","sellerCompanyFeedbackCount","companyStateReraRegistrationId"],"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"listingCategory":["Rental"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
+let rent = '';
+request(renturl,function(err,response,body) {
+	rent = JSON.parse(body);
+});
+app.get('/rent',function(req,res) {
+	res.render('index.ejs',{rent:rent.data[0].facetedResponse});
+});
+
+
+app.listen(3000,function(){
+	console.log("Server listening at port 3000...");
+})
---
+++ public/script.js
@@ -0,0 +1,29 @@
+
+
+// function myFunction('mydropdown') {
+//     document.getElementById('mydropdown').classList.toggle("show");
+
+// }
+
+// window.onclick = function(e) {
+//   if (!e.target.matches('.buy')) {
+//     var myDropdown = document.getElementById(id);
+//       if (myDropdown.classList.contains('show')) {
+//         myDropdown.classList.remove('show');
+//       }
+//   }
+// }
+
+// function change()
+// {
+// 	document.getElementById('shortlist').innerHTML = "<img src="/images/bookmark1.png">";
+// }
+
+function changeImage() {
+					var image = document.getElementById('shortlist');
+					 if (image.src=="/images/bookmark.png") {
+						   image.src = "/images/bookmark1.png";
+					 } else {
+						 image.src = "/images/bookmark.png";
+							}
+					}
